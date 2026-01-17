38ef82b6ab5e4015df53e3c1377f05b5aab92508
lilbrojoe12 <harris.jordan5305@gmail.com>
2026-01-17 12:43:31 -0700

    Update mission.js

--- mission/mission.js
+++ mission/mission.js
@@ -1,15 +1,20 @@
-
 let selectElem = document.querySelector('select');
 let logo = document.querySelector('img');
+let titleText = document.getElementById('para1');
 
 selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-        document.body.style.backgroundColor = 'black';
-        document.body.style.color = 'white'; // code for changes to colors and logo
+        document.body.style.backgroundColor = '#39393a';
+        document.body.style.color = 'white';
+        logo.src = 'byui-logo-white.png';
+        titleText.style.color = '#a5c7e0'; // code for changes to colors and logo
     } else {
-        // code for changes to colors and logo
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+        logo.src = 'byui-logo.png';
+        titleText.style.color = '#035f9c';// code for changes to colors and logo
     }
 }
