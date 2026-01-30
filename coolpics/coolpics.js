ab6a873b689073bf4d5c1cba49bf4ab405b6405f
lilbrojoe12 <harris.jordan5305@gmail.com>
2026-01-29 20:37:09 -0700

    update

---
+++ coolpics/coolpics.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('nav');
+
+btn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle('hide');
+}
