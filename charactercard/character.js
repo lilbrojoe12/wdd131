af4fde37528bccb3e771418000c08eb72672d4c1
lilbrojoe12 <harris.jordan5305@gmail.com>
2026-02-25 13:56:23 -0700

    update

---
+++ charactercard/character.js
@@ -0,0 +1,41 @@
+const character = {
+  name: "Snortleblat",
+  class: "Swamp Beast Diplomat",
+  level: 5,
+  health: 100,
+  image: "snortleblat.webp",
+
+  attacked() {
+    this.health -= 20;
+
+    if (this.health <= 0) {
+      this.health = 0;
+      alert(`${this.name} has died!`);
+    }
+
+    updateCard();
+  },
+
+  levelUp() {
+    this.level += 1;
+    updateCard();
+  }
+};
+
+const healthElement = document.querySelector(".stats p:nth-child(3)");
+const levelElement = document.querySelector(".stats p:nth-child(2)");
+
+function updateCard() {
+  levelElement.innerHTML = `<strong>Level:</strong> ${character.level}`;
+  healthElement.innerHTML = `<strong>Health:</strong> ${character.health}`;
+}
+
+const buttons = document.querySelectorAll("button");
+
+buttons[0].addEventListener("click", function () {
+  character.attacked();
+});
+
+buttons[1].addEventListener("click", function () {
+  character.levelUp();
+});
